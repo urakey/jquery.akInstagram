1487cf90e562c85ddc05e2b85fcee5e9ecaceaed
isao 'home' haba <isao@trapon.com>
2015-11-27 20:48:32 +0900

    INPUT_SERVERで取得できない場合、INPUT_ENVで取得するように
     - filter_inputでINPUT_SERVERの値がCGIで動いている場合とれないかも
     - INPUT_ENVでとるようにした
     - それにともなって、$access_tokenと$request_urlを外に出した

--- instagram.php
+++ instagram.php
@@ -1,7 +1,10 @@
 <?php
-  if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'GET'){
     $access_token = '211242.....';
     $request_url  = 'https://api.instagram.com/v1/users/self/media/recent/';
+  if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'GET'){
+    echo @file_get_contents($request_url.'?access_token='.$access_token);
+    exit;
+  } elseif (filter_input(INPUT_ENV, 'REQUEST_METHOD') == 'GET'){
     echo @file_get_contents($request_url.'?access_token='.$access_token);
     exit;
   }
