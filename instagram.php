33c981102167a780357c758f3053cfe465a2312c
isao 'home' haba <isao@trapon.com>
2015-11-27 22:07:11 +0900

    access_token を token.php に出した
     - 可搬性(dev,productionなどの環境の違い)を踏まえてaccess_tokenを別ファイルにした
     -- token.php にaccess_tokenを定義
     -- require で読み込みパスとファイル名を指定
     -- .gitignore でtoken.phpを除外
     -- token.php.sample でtoken.phpのサンプルを用意

--- instagram.php
+++ instagram.php
@@ -1,5 +1,8 @@
 <?php
-    $access_token = '211242.....';
+  $include_path = './';
+  $token_file = 'token.php';
+  require($include_path . $token_file);
+  $access_token = ACCESS_TOKEN;
     $request_url  = 'https://api.instagram.com/v1/users/self/media/recent/';
   if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'GET'){
     echo @file_get_contents($request_url.'?access_token='.$access_token);
